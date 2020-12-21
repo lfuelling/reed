dea9fc847792ae658f1643c903796256769bfeaa
Lukas Fülling <lukas@k40s.net>
2020-12-21 16:47:07 +0100

    Initial Commit

---
+++ reed/reedApp.swift
@@ -0,0 +1,17 @@
+//
+//  reedApp.swift
+//  reed
+//
+//  Created by Lukas Fülling on 21.12.20.
+//
+
+import SwiftUI
+
+@main
+struct reedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
