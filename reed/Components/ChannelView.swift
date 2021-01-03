fd846410c78161bd6bc443b4bcdfacfa22077907
Lukas Fülling <lukas@k40s.net>
2021-01-03 03:23:53 +0100

    fix layout of article list

--- reed/Components/ChannelView.swift
+++ reed/Components/ChannelView.swift
@@ -30,51 +30,59 @@ struct ChannelView: View {
     var body: some View {
         if articles.count > 0 {
             List(articles, id: \.self.id, selection: $selectedArticle) { article in
-                HStack(alignment: .top) {
-                    if !article.read {
-                        Image(systemName: "circle.fill")
-                            .foregroundColor(.accentColor)
-                            .font(.footnote)
-                    } else {
-                        if article.bookmarked {
-                            Image(systemName: "star.fill")
-                                .foregroundColor(.yellow)
-                                .font(.footnote)
-                        } else {
-                            Image(systemName: "circle.fill")
-                                .foregroundColor(.accentColor)
-                                .font(.footnote)
-                                .opacity(0.0)
-                        }
-                    }
-                    NavigationLink(
-                        destination: ArticleView(article: article, channel: channel, persistenceProvider: persistenceProvider, refreshData: refreshData, updater: $updater),
-                        tag: article,
-                        selection: $selectedArticle
-                    ) {
+                VStack(alignment: .leading) {
+                    HStack(alignment: .center) {
                         VStack(alignment: .leading) {
-                            HStack {
-                                Text(article.title!)
-                                    .font(.headline)
-                                    .lineLimit(1)
-                                if let date = article.date {
-                                    Spacer()
-                                    Text(date, style: .date)
-                                        .font(.subheadline)
+                            if !article.read {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.accentColor)
+                                    .font(.footnote)
+                            } else {
+                                if article.bookmarked {
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                        .font(.footnote)
+                                } else {
+                                    Image(systemName: "circle.fill")
+                                        .foregroundColor(.accentColor)
+                                        .font(.footnote)
+                                        .opacity(0.0)
+                                }
+                            }
+                        }
+                        NavigationLink(
+                            destination: ArticleView(article: article, channel: channel, persistenceProvider: persistenceProvider, refreshData: refreshData, updater: $updater),
+                            tag: article,
+                            selection: $selectedArticle
+                        ) {
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text(article.title!)
+                                        .font(.headline)
+                                        .lineLimit(1)
+                                    if let date = article.date {
+                                        Spacer()
+                                        Text(date, style: .date)
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                    }
+                                }
+                                
+                                if let description = article.articleDescription {
+                                    Text(description.unhtml())
+                                        .font(.callout)
+                                        .lineLimit(Int(descriptionMaxLines))
                                         .foregroundColor(.secondary)
                                 }
                             }
-                            
-                            if let description = article.articleDescription {
-                                Text(description.unhtml())
-                                    .font(.callout)
-                                    .lineLimit(Int(descriptionMaxLines))
-                                    .foregroundColor(.secondary)
-                            }
                         }
+                    }.padding(4)
+                    if(selectedArticle?.id != article.id) {
+                        Divider()
+                    } else {
+                        Divider().opacity(0)
                     }
-                }
-                Divider()
+                }.padding(0)
             }.navigationTitle(channel.title ?? "untitled")
             .navigationSubtitle(channel.channelDescription ?? "")
         } else {
